cd847d81316be44205bfa6538e640d45c492f31f
Lalit Dadaji Borse <157976243+LalitBorse14@users.noreply.github.com>
2024-12-02 21:31:03 +0530

    Update create-and-upload-to-s3.py

--- Scripts/create-and-upload-to-s3.py
+++ Scripts/create-and-upload-to-s3.py
@@ -1,17 +1,31 @@
 #Create s3 bucket 
 import boto3
 
-s3 = boto3.client('s3')
-bucket_name = 'my-automation-bucket11'
-s3.create_bucket(Bucket=bucket_name)
-print(f'Bucket {bucket_name} created.')
+# Initialize the S3 client
+s3 = boto3.client('s3', region_name='ap-south-1')
+
+# Bucket name
+bucket_name = 'lalitborse12345'
+
+try:
+    # Create the S3 bucket with the correct region
+    response = s3.create_bucket(
+        Bucket=bucket_name,
+        CreateBucketConfiguration={
+            'LocationConstraint': 'ap-south-1'
+        }
+    )
+    print(f"Bucket {bucket_name} created successfully!")
+except Exception as e:
+    print(f"Error: {e}")
+
 
 
 # Upload file to s3 bucket
 import boto3
 
 s3 = boto3.client('s3')
-bucket_name = 'my-automation-bucket11'
+bucket_name = 'lalitborse12345'
 file_name = 'myfile.txt'
 
 s3.upload_file(file_name, bucket_name, file_name)
